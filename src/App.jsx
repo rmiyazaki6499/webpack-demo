f95883c73430cf9bebdfd66adf1b2fca04c33928
Ryuichi Miyazaki <rmiyazaki@Ryuichis-MacBook-Pro.local>
2025-03-18 10:50:36 +0700

    🎉 Creating webpack demo project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Stop crying you baby!</h1>
+      <img src="https://prod-hk.oss-eu-central-1.aliyuncs.com/default/20240925_131351_333648____2_____1200x1200.jpg?x-oss-process=image/format,webp"></img>
+    </>
+  );
+};
+
+export default App;
